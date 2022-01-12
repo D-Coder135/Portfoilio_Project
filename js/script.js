64add1da7aa2d7438aef0415367b3666f0ff79c5
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-12 21:58:56 +0530

    initialize a const field

--- js/script.js
+++ js/script.js
@@ -2,9 +2,11 @@
 
 const tabsContainer = document.querySelector(".about-tabs"),
 aboutSection = document.querySelector(".about-section");
+
 tabsContainer.addEventListener("click", (e) => {
     if(e.target.classList.contains("tab-item") && !e.target.classList.contains("active")){
         tabsContainer.querySelector(".active").classList.remove("active");
         e.target.classList.add("active");
-    };
+        const target = e.target.getAttribute("data-target");
+    }
 });
