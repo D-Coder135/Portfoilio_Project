c8f95258627943ed7798f7c32214e8a844830143
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-12 21:52:32 +0530

    call the console.log() method

--- js/script.js
+++ js/script.js
@@ -3,5 +3,5 @@
 const tabsContainer = document.querySelector(".about-tabs"),
 aboutSection = document.querySelector(".about-section");
 tabsContainer.addEventListener("click", (e) => {
-    
+    console.log(e.target);
 });
