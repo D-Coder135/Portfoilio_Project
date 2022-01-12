e484df96fe2b157e97b5e3693a86fd3b6ae43dd4
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-12 21:51:18 +0530

    initialize another const field

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
 // About Tabs
 
-const tabsContainer = document.querySelector(".about-tabs");
+const tabsContainer = document.querySelector(".about-tabs"),
+aboutSection = document.querySelector(".about-section");
