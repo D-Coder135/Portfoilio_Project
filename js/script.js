373372d60b484f1f0c9ba4e52f76438c20510327
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-15 19:43:26 +0530

    call the toggleNavbar() function

--- js/script.js
+++ js/script.js
@@ -23,7 +23,7 @@ document.addEventListener("click", (e) => {
     if(e.target.classList.contains("link-item") && e.target.hash !== "") {
         const hash = e.target.hash;
         if(e.target.classList.contains("nav-item")) {
-            
+            toggleNavbar();
         }
     }
 });
