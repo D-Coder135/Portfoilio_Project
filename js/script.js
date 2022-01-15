2eec37d9914e1eac73da1e0fd2b1493a3be64cad
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-15 21:02:09 +0530

    call the querySelector().classList.add() method

--- js/script.js
+++ js/script.js
@@ -21,6 +21,7 @@ function toggleNavbar() {
 
 document.addEventListener("click", (e) => {
     if(e.target.classList.contains("link-item") && e.target.hash !== "") {
+        document.querySelector(".overlay").classList.add("active");
         navToggler.classList.add("hide")
         if(e.target.classList.contains("nav-item")) {
             toggleNavbar();
