6b5dfa254330724891b523ef67e049572ce3094a
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-16 15:44:54 +0530

    call the querySelector.classList.add() nethod

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ window.addEventListener("load" , () => {
     document.querySelector(".main").classList.remove("hidden");
     document.querySelector(".home-section").classList.add("active");
     // Page Loader
+    document.querySelector(".page-loader").classList.add("fade-out");
 });
 
 // Toggle Navbar 
