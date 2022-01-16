93e0050399d101823bd6d33299f8020b3c14a4bf
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-01-16 15:44:23 +0530

    add some comment to the code

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 window.addEventListener("load" , () => {
     document.querySelector(".main").classList.remove("hidden");
     document.querySelector(".home-section").classList.add("active");
-    
+    // Page Loader
 });
 
 // Toggle Navbar 
