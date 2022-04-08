8985938569c9988e7bffe703d10b281515e2c3a8
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-04-09 00:02:33 +0530

    add some attributes for the form tag

--- js/script.js
+++ js/script.js
@@ -107,7 +107,15 @@ function portfolioItemDetails(portfolioItem) {
 document.querySelector(".contact-form").addEventListener("submit", sendEmail);
 
 function sendEmail() {
-    
-
-    
+    Email.send({
+        Host : "smtp.yourisp.com",
+        Username : "username",
+        Password : "password",
+        To : 'them@website.com',
+        From : "you@isp.com",
+        Subject : "This is the subject",
+        Body : "And this is the body"
+    }).then(
+      message => alert(message)
+    );
 }
