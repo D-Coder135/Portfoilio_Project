3664d0c6c6576297fcfe493dba6c307a6c2a08f3
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-04-09 00:23:07 +0530

    comment out some part of the code

--- js/script.js
+++ js/script.js
@@ -104,18 +104,18 @@ function portfolioItemDetails(portfolioItem) {
 }
 
 // Listen For Send Message
-document.querySelector(".contact-form").addEventListener("submit", sendEmail);
+// document.querySelector(".contact-form").addEventListener("submit", sendEmail);
 
-function sendEmail() {
-    Email.send({
-        Host : "smtp.yourisp.com",
-        Username : "username",
-        Password : "password",
-        To : 'them@website.com',
-        From : "you@isp.com",
-        Subject : "This is the subject",
-        Body : "And this is the body"
-    }).then(
-      message => alert(message)
-    );
-}
+// function sendEmail() {
+//     Email.send({
+//         Host : "smtp.yourisp.com",
+//         Username : "username",
+//         Password : "password",
+//         To : 'them@website.com',
+//         From : "you@isp.com",
+//         Subject : "This is the subject",
+//         Body : "And this is the body"
+//     }).then(
+//       message => alert(message)
+//     );
+// }
